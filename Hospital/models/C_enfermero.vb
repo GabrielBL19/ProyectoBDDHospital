0b471062b2ff7af7d55bddbe478cfee07343ba70
donal <donaldovarela2002@gmail.com>
2024-11-10 01:18:43 -0600

    clase enfermero

--- Hospital/models/C_enfermero.vb
+++ Hospital/models/C_enfermero.vb
@@ -2,7 +2,7 @@
     Inherits C_persona
     Public Property idCargo As Integer
     Public Property estadoCivil As Integer
-    Public Property estado As Integer
     Public Property titulo As String
+    Public Property estado As Integer
     Public Property sueldo As Double
 End Class
